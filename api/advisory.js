3d0977259157c65ad270a6761ef92827842e8e8e
Vitaliy Gorpenko <vitalgorwork@gmail.com>
2026-03-09 14:40:44 -0700

    Advisory corrected

--- api/advisory.js
+++ api/advisory.js
@@ -1,10 +1,9 @@
-// api/advisory.js
 module.exports = async (req, res) => {
-  const qs = req.url.split('?')[1] || '';
-  const upstream = `http://www.travel-advisory.info/api${qs ? '?' + qs : ''}`;
-  const r = await fetch(upstream);
-  const body = await r.text();
-  res.setHeader('Content-Type', 'application/json');
-  res.setHeader('Cache-Control', 's-maxage=3600, stale-while-revalidate');
-  res.status(r.status).send(body);
+    const qs = req.url.split('?')[1] || '';
+    const upstream = `http://www.travel-advisory.info/api${qs ? '?' + qs : ''}`;
+    const r = await fetch(upstream);
+    const body = await r.text();
+    res.setHeader('Content-Type', 'application/json');
+    res.setHeader('Cache-Control', 's-maxage=3600, stale-while-revalidate');
+    res.status(r.status).send(body);
 };
